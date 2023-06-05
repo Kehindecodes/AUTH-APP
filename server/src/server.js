9030aae927a4091eb8940ac49410dcf1e14b6b1c
Oki Kehinde <olamiposi.oki@gmail.com>
2023-06-05 21:28:29 +0100

    setup database connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,13 @@
 const http = require('http');
 const app = require('./app');
 require('dotenv').config();
-// const { mongoConnect } = require('./services/mongo');
+const { mongoConnect } = require('./connect');
 const server = http.createServer(app);
-
-
 
 const PORT = process.env.PORT || 8080;
 
 async function startServer() {
-	
+	await mongoConnect();
 	server.listen(PORT, () => {
 		console.log(`Listening on port ${PORT}`);
 	});
