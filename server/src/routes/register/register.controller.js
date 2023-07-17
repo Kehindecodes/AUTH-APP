c1558c1a000984d3cd0ba8831c7252631dcbe7b2
Oki Kehinde <olamiposi.oki@gmail.com>
2023-07-17 21:53:53 +0100

    store only email and password

--- server/src/routes/register/register.controller.js
+++ server/src/routes/register/register.controller.js
@@ -7,7 +7,7 @@ const { base32 } = require('hi-base32');
 
 async function registerUser(req, res) {
 	try {
-		const { name, email, password } = req.body;
+		const { email, password } = req.body;
 
 		// Check if a user with the provided email already exists
 		const existingUser = await User.findOne({ email });
@@ -23,12 +23,6 @@ async function registerUser(req, res) {
 			return randomId;
 		}
 
-		// const generateSecretKey = () => {
-		// 	const secretKey = OTPAuth.Secret.generate({ length: 20 }); // Generate a 20-byte secret key
-		// 	const base32Key = base32.encode(secretKey.buffer);
-		// 	return base32Key;
-		// };
-
 		const user = new User({
 			_id: generateRandomId(),
 			name,
@@ -36,28 +30,17 @@ async function registerUser(req, res) {
 			password: hash,
 			// secretKey: generateSecretKey(),
 		});
-		const payload = { sub: user._id };
-		const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-			expiresIn: '1h',
-		});
 
 		await user.save();
+		console.log('user is registered successfully');
+		res.json({ redirectTo: '/login' });
+		// res.status(201).json({
+		// 	message: 'User successfully created',
+		// 	user,
+		// 	token,
+		// });
 
-		// const generateOTPUrl = (user) => {
-		// 	const otpauthURL = OTPAuth.URI.stringify({
-		// 	  type: 'totp',
-		// 	  secret: base32.decode(user.secretKey),
-		// 	  label: 'Auth app:' + user.email,
-		// 	  issuer: 'Auth app',
-		// 	});
-		// 	return otpauthURL;
 		//   };
-
-		res.status(201).json({
-			message: 'User successfully created',
-			user,
-			token,
-		});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
