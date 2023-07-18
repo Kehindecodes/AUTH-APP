a4e0147fa157880b33302c61a689c41f36dcd4ff
Oki Kehinde <olamiposi.oki@gmail.com>
2023-07-18 22:28:31 +0100

    implement login functionality

--- client/Auth-app/src/pages/LoginPage.jsx
+++ client/Auth-app/src/pages/LoginPage.jsx
@@ -13,16 +13,35 @@ import {
 	InputGroup,
 	Button,
 } from '@chakra-ui/react';
+import axios from 'axios';
 import { LockIcon, EmailIcon } from '@chakra-ui/icons';
 import logo from '../assets/devchallenges.svg';
 import FormComponent from '../components/Form';
+import { useNavigate } from 'react-router-dom';
 const LoginPage = () => {
-    const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	});
 	const [show, setShow] = useState(false);
+	const navigate = useNavigate();
 	const handleClick = () => setShow(!show);
+	const handleSubmit = async (email,password) => {
+	
+		try {
+		  const response = await axios.post('http://127.0.0.1:8080/auth/login', { email, password });
+	
+		  if (response.status === 200) {
+			console.log('User is logged in successfully');
+	
+			if (response.data.redirectTo) {
+			  navigate(response.data.redirectTo); // Redirect to the specified page
+			} else {
+			  // Handle successful login without redirectTo property
+			}
+		  }
+		} catch (error) {
+			console.error(error);
+
+		  
+		}
+	  };
   return (
     <Box
         bg='#fff'
@@ -36,6 +55,7 @@ const LoginPage = () => {
 					handleClick={handleClick}
 					show={show}
 					loginState={true}
+					onSubmit={handleSubmit}
 				/>
 			</Center>
     </Box>
